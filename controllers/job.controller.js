83336cba039fdf4a5fe3799c7d2d7095a81cada0
Your Sp2089 Username <youremail@example.com>
2025-07-04 11:12:24 +0530

    Force add frontend/dist for Render deployment

--- controllers/job.controller.js
+++ controllers/job.controller.js
@@ -62,9 +62,14 @@ export const getAllJobs = async (req, res) => {
 export const getJobById = async (req,res) => {
     try {
         const jobId = req.params.id;
-        const job = await Job.findById(jobId).populate({
-            path:"applications",
-        });
+        const job = await Job.findById(jobId).populate([
+            {
+                path: "company"
+            },
+            {
+                path: "applications"
+            }
+        ]);
         if(!job) return res.status(404).json({
             message:"Job not found.",
             success:false
